403f5752c65effd1411fdd33a3852fb8e857c864
liubingjie <breeder1121@hotmail.com>
2017-05-10 16:38:47 +0800

    实现购物车统计总价功能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    @groduct = Product.find(params[:id])
+    @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
     flash[:notice] = "成功加入购物车"
     redirect_to :back
