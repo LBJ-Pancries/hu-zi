4372cbfc928c2320f7ee7e52148ca7700552addd
liubingjie <breeder1121@hotmail.com>
2017-05-18 16:56:33 +0800

    用户可以看到他们的订单

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,7 @@ Rails.application.routes.draw do
 
   resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ namespace :account do
+   resources :orders
+ end
 end
