01c0f32b87fa81ffba86e314934a1a7b29509b44
liubingjie <breeder1121@hotmail.com>
2017-05-20 20:23:21 +0800

    在出货／取消订单后，系统应该寄出通知信

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,9 +8,15 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.order("id DESC")
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
   def ship
     @order = Order.find(params[:id])
     @order.ship!
+    OrderMailer.notify_ship(@order).deliver!
     redirect_to :back
   end
 
@@ -23,6 +29,7 @@ class Admin::OrdersController < ApplicationController
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
