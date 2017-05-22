cd0894f0c4eefd7f2720040c8cd0ec689a03a3ed
liubingjie <breeder1121@hotmail.com>
2017-05-22 20:35:18 +0800

    modify uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  storage :qiniu
+  if Rails.env.production?
+    storage :qiniu
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -15,14 +19,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [600, 800]
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [150,200]
   end
 
   version :medium do
-    process resize_to_fill: [400,400]
+    process resize_to_fill: [300,400]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
