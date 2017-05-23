d834a482e68510b54214e6082377b02af08855c7
liubingjie <breeder1121@hotmail.com>
2017-05-23 09:25:32 +0800

    config qiniu

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["lDbFzfFhBXIAMlrU_B0Noxf1fk3wWILGo5ZNOwxC"]
-  config.qiniu_secret_key    = ENV["ZoF0r6Q71ChAbwZIgEJh2ZMCmVCRl9Rtfo1Sjylu"]
-  config.qiniu_bucket        = ENV["dog-book"]
-  config.qiniu_bucket_domain = ENV["oqcq4n1q1.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
