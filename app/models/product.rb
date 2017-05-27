ac15fcbc2deb00637184446107437925146ba434
liubingjie <breeder1121@hotmail.com>
2017-05-27 17:29:00 +0800

    优化购物车index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   validates :image, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
    mount_uploader :image, ImageUploader
    acts_as_list
 end
