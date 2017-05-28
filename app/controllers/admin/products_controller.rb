b52580ff1c94ea67ab5f900ac0c3fc5a799630c8
liubingjie <breeder1121@hotmail.com>
2017-05-28 14:00:44 +0800

    实作商品分类功能

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,10 +9,12 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def create
     @product = Product.new(product_params)
+    @product.category_id = params[:category_id]
 
     if @product.save
       redirect_to admin_products_path
@@ -23,10 +25,12 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def update
     @product = Product.find(params[:id])
+    @product.category_id = params[:category_id]
 
     if @product.update(product_params)
       redirect_to admin_products_path
@@ -58,6 +62,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :writer, :description, :quantity, :price, :image, :press, :subtitle, :translator, :year, :pages, :ISBN, :about_author)
+    params.require(:product).permit(:title, :writer, :description, :quantity, :price, :image, :press, :subtitle, :translator, :year, :pages, :ISBN, :about_author, :category_id)
   end
 end
