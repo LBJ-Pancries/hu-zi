7fbbbf9bf5d1906f4d523e51329e65e52a25beaf
liubingjie <breeder1121@hotmail.com>
2017-06-03 14:49:32 +0800

    第三方登陆步骤二

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  devise_for :users, :controller => {:omniauth_callbacks => "omniauth_callbacks"}
+  devise_for :users, :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
 
   namespace :admin do
     resources :products do
