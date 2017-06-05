67d4fb18fd8bc16a91721f2042e7b31215a0f520
liubingjie <breeder1121@hotmail.com>
2017-06-05 17:35:07 +0800

    create groups index

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,6 @@ Rails.application.routes.draw do
    resources :orders
  end
 
+ resources :groups
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605025248) do
+ActiveRecord::Schema.define(version: 20170605092729) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20170605025248) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "identifies", force: :cascade do |t|
