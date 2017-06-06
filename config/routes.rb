b22aaf4ae3a1ccf5f5373efa412ac9ae02aa33d3
liubingjie <breeder1121@hotmail.com>
2017-06-06 11:37:41 +0800

    实做在自己后台看到自己发表的文章以及创立的社团

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ Rails.application.routes.draw do
   resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  namespace :account do
+   resources :groups
+   resources :posts
    resources :orders
  end
 
