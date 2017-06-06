43114838cdbde6a7cd1ac4a73756efc8b06e8b0b
liubingjie <breeder1121@hotmail.com>
2017-06-06 19:38:43 +0800

    实作posts的edit/delete功能

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170606015016) do
 
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.string   "rateable_type"
-    t.integer  "rateable_id"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
@@ -86,14 +77,6 @@ ActiveRecord::Schema.define(version: 20170606015016) do
     t.index ["aasm_state"], name: "index_orders_on_aasm_state"
   end
 
-  create_table "overall_averages", force: :cascade do |t|
-    t.string   "rateable_type"
-    t.integer  "rateable_id"
-    t.float    "overall_avg",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "group_id"
@@ -129,29 +112,6 @@ ActiveRecord::Schema.define(version: 20170606015016) do
     t.integer  "ISBN"
     t.text     "about_author"
     t.integer  "category_id"
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.string   "rateable_type"
-    t.integer  "rateable_id"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
-    t.index ["rater_id"], name: "index_rates_on_rater_id"
-  end
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.string   "cacheable_type"
-    t.integer  "cacheable_id"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
   end
 
   create_table "users", force: :cascade do |t|
