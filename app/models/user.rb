35da7d9ac711db5b98e3a617f2092ecd833cd9a1
liubingjie <breeder1121@hotmail.com>
2017-06-06 10:57:50 +0800

    实作加入／退出社群功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,23 @@ class User < ApplicationRecord
 
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
 
   def admin?
     is_admin
+  end
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
   end
 
   def self.from_google(access_token, signed_in_resource=nil)
--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,10 @@ Rails.application.routes.draw do
  end
 
  resources :groups do
+   member do
+     post :join
+     post :quit
+   end
    resources :posts
  end
 
