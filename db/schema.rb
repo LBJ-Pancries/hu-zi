c829bac01ef548f65413dcd75be8b8466429bb80
liubingjie <breeder1121@hotmail.com>
2017-06-09 16:41:59 +0800

    增加薪资／联系方式栏位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608065027) do
+ActiveRecord::Schema.define(version: 20170609082935) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -64,8 +64,11 @@ ActiveRecord::Schema.define(version: 20170608065027) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "orders", force: :cascade do |t|
